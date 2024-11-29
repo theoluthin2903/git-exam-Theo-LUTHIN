b0bbb098c5c59e1f65dac5d4631c5e9d4d3ccfdb
oue <bleumisc@gmail.com>
2024-11-29 16:01:24 +0100

    added feature url

--- main.go
+++ main.go
@@ -1,12 +1,17 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 )
 
+func IndexHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "route: %q", r.URL.Path)
+}
+
 func main() {
-	// to add : url functionality
+	http.HandleFunc("/", IndexHandler)
 	// to add : color functionality
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
