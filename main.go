a8611fba8ff4f880c36dba035e88d54e0e69fe61
tluthin67 <theo.luthin2903@gmail.com>
2025-12-05 13:47:05 +0100

    Add ColorHandler for color route

--- main.go
+++ main.go
@@ -10,8 +10,13 @@ func IndexHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "route: %q", r.URL.Path)
 }
 
+func ColorHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "<h1>Colors</h1><style>*{background-color: #006400;}</style>")
+}
+
 func main() {
 	http.HandleFunc("/", IndexHandler)
+	http.HandleFunc("/color", ColorHandler)
 	// to add : color functionality
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
